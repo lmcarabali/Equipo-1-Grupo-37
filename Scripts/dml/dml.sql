d579ed78b6b8a8a8b1bdb96a30426d73701e74bf
Carolina Lopez <>
2022-09-12 22:09:20 -0500

    Script de base de datos
    Se crea script para la creacion de base de datos y las tablas con sus llaves e indices

---
+++ Scripts/dml/dml.sql
@@ -0,0 +1,72 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.10.1-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for tecnoshop
+CREATE DATABASE IF NOT EXISTS `tecnoshop` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `tecnoshop`;
+
+-- Dumping structure for table tecnoshop.productos
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id_producto` int(11) NOT NULL AUTO_INCREMENT,
+  `marca` varchar(100) NOT NULL,
+  `modelo` varchar(100) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `precio` double NOT NULL,
+  `descripcion_adicional` varchar(5000) DEFAULT NULL,
+  `stock` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  `categoria` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_producto`),
+  KEY `Productos_categoria` (`categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table tecnoshop.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `numero_identificacion` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `direccion` varchar(300) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `numero_identificacion` (`numero_identificacion`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table tecnoshop.venta
+CREATE TABLE IF NOT EXISTS `venta` (
+  `id_venta` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `valor` double NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`id_venta`),
+  KEY `fk_producto` (`id_producto`),
+  KEY `fk_usuario` (`id_usuario`),
+  CONSTRAINT `fk_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`),
+  CONSTRAINT `fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
